abd86380fdb024b35ff580e2b5a9f8bd95b9936e
Marcos Vinicius <marcosrodrigues993@hotmail.com>
2024-07-16 19:52:05 +0100

    entity order, enum, many-to-one

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,12 +1,17 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import org.springframework.web.bind.annotation.Mapping;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 	@Entity
@@ -21,6 +26,9 @@ import jakarta.persistence.Table;
 		private String phone;
 		private LocalDate birthDate;
 		private String password;
+		
+		@OneToMany(mappedBy = "client")
+		private List<Order> orders = new ArrayList<>();
 		
 		public User() {
 		}
