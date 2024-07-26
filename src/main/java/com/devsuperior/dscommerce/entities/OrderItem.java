9adc5d453e9ff3ff04a0b6dae9a2eaea5eb0cf23
Marcos Vinicius <marcosrodrigues993@hotmail.com>
2024-07-26 15:16:09 +0100

    Database seeding

---
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItem.java
@@ -0,0 +1,62 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem { 
+ 
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
+	
+	private Integer quantity;
+	private Double price;
+	
+	
+	public OrderItem(Order order, Product product, int quantity, Double price) {
+		super();
+		this.id.setOrder(order);
+		this.id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void serOrder(Order order) {
+		this.id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void serProduct(Product product) {
+		this.id.setProduct(product);
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	
+}
