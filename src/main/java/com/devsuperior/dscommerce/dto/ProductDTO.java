7e25f3c6866beb04e9d0a8c87058044ff936f83b
Marcos Vinicius <marcosrodrigues993@hotmail.com>
2024-10-05 11:24:46 -0300

    Fix validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.devsuperior.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -22,6 +23,7 @@ public class ProductDTO {
 	@Size(min = 10, message = "A descricao precisa de no minimo 10 caracteres")
 	private String description;
 	
+	@NotNull(message = "Campo requerido")
 	@Positive(message = "O valor precisa ser positivo")
 	private Double price;
 	private String imgUrl;
